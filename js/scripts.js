fc07c29ec4ce09ce13edf9e345f519537d893fe7
Stuart McKay <stuart51994gmail.comgit config --global user.email stuart51994@gmail.com>
2019-03-29 14:39:15 -0700

    remove <label> tags around <input> fields to have jQuery recognize changes without submit button req.Have full functional linking with value recognition in console.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,28 @@
+// $(document).ready(function() {
+//   $("form.question1").submit(function(event) {
+//     event.preventDefault();
+//     var answer1 = $('q1').val();
+//     console.log(answer1);
+//   });
+// });
 $(document).ready(function() {
-  $("input[type='button']").click(function(){
-    var radioValue = $("input[name=q1Answer]:checked").val();
-    var answer1 = q1Answer;
-    var answer2 = q2Answer;
-    var answer3 = q3Answer;
-    var answer4 = q4Answer;
-    var answer5 = q5Answer;
-
-    console.log(q1Answer)
-    console.log(q2Answer)
-    console.log(q3Answer)
-    console.log(q4Answer)
-    console.log(q5Answer)
+  $("form#q1Input").change(function(){
+    var answer1 = $("input[name='q1Answer']:checked").val();
+    console.log(answer1);
   });
 });
+// var answer2 = q2Answer;
+// console.log(q2Answer);
+//
+// var answer3 = q3Answer;
+// console.log(q3Answer);
+//
+// var answer4 = q4Answer;
+// console.log(q4Answer);
+//
+// var answer5 = q5Answer;
+// console.log(q5Answer);
+
       // alert("You might be best suited for these tracks: ");
       // alert(answer1);
       // alert(answer2);
