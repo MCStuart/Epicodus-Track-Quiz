fe7cf6343ac046d642ca4a036434932ca7fc0e43
Stuart McKay <stuart51994gmail.comgit config --global user.email stuart51994@gmail.com>
2019-03-29 16:06:08 -0700

    reinitialize git and push

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,108 @@
 $(document).ready(function() {
+  var cSharpCnt = 0;
+  var javaCnt = 0;
+  var rubyCnt = 0;
+  var phpCnt = 0;
+
   $("form#q1Input").change(function(){
     var answer1 = $("input[name='q1Answer']:checked").val();
     console.log(answer1);
+    if (answer1 === "ruby") {
+      rubyCnt++;
+    } else if (answer1 === "cSharp") {
+      cSharpCnt++;
+    } else if (answer1 === "java") {
+      javaCnt++;
+    } else if (answer1 === "php") {
+      phpCnt++;
+    }
   });
   $("form#q2Input").change(function(){
     var answer2 = $("input[name='q2Answer']:checked").val();
     console.log(answer2);
+    if (answer1 === "ruby") {
+      rubyCnt++;
+    } else if (answer1 === "cSharp") {
+      cSharpCnt++;
+    } else if (answer1 === "java") {
+      javaCnt++;
+    } else if (answer1 === "php") {
+      phpCnt++;
+    }
   });
   $("form#q3Input").change(function(){
     var answer3 = $("input[name='q3Answer']:checked").val();
     console.log(answer3);
+    if (answer1 === "ruby") {
+      rubyCnt++;
+    } else if (answer1 === "cSharp") {
+      cSharpCnt++;
+    } else if (answer1 === "java") {
+      javaCnt++;
+    } else if (answer1 === "php") {
+      phpCnt++;
+    }
   });
   $("form#q4Input").change(function(){
     var answer4 = $("input[name='q4Answer']:checked").val();
     console.log(answer4);
+    if (answer1 === "ruby") {
+      rubyCnt++;
+    } else if (answer1 === "cSharp") {
+      cSharpCnt++;
+    } else if (answer1 === "java") {
+      javaCnt++;
+    } else if (answer1 === "php") {
+      phpCnt++;
+    }
   });
-  $("form#q5Input").change(function(){
+  $("form#q5Input").change(function() {
     var answer5 = $("input[name='q5Answer']:checked").val();
     console.log(answer5);
+    if (answer1 === "ruby") {
+      rubyCnt++;
+    } else if (answer1 === "cSharp") {
+      cSharpCnt++;
+    } else if (answer1 === "java") {
+      javaCnt++;
+    } else if (answer1 === "php") {
+      phpCnt++;
+    }
   });
+
+  function decideLanguage(cSharpCnt, javaCnt, rubyCnt, phpCnt) {
+    if (cSharpCnt >= 3) {
+      $("#cSharpCard").show();
+      $("#javaCard").hide();
+      $("#phpCard").hide();
+      $("#rubyCard").hide();
+      $(".questionField").hide();
+    }
+    if (javaCnt >= 3) {
+      $("#cSharpCard").hide();
+      $("#javaCard").show();
+      $("#phpCard").hide();
+      $("#rubyCard").hide();
+      $(".questionField").hide();
+    }
+    if (phpCnt >= 3) {
+      $("#cSharpCard").hide();
+      $("#javaCard").hide();
+      $("#phpCard").show();
+      $("#rubyCard").hide();
+      $(".questionField").hide();
+    }
+    if (rubyCnt >= 3) {
+      $("#cSharpCard").hide();
+      $("#javaCard").hide();
+      $("#phpCard").hide();
+      $("#rubyCard").show();
+      $(".questionField").hide();
+    }
+  };
+    // else ;
+    //   $("#javaCard").show();
+    //   $(".questionField").hide();
+  // };
+  decideLanguage(cSharpCnt, javaCnt, rubyCnt, phpCnt);
 });
-
-// var answer2 = q2Answer;
-// console.log(q2Answer);
-//
-// var answer3 = q3Answer;
-// console.log(q3Answer);
-//
-// var answer4 = q4Answer;
-// console.log(q4Answer);
-//
-// var answer5 = q5Answer;
-// console.log(q5Answer);
-
-      // alert("You might be best suited for these tracks: ");
-      // alert(answer1);
-      // alert(answer2);
-      // alert(answer3);
-      //
-      // if (answer1 != answer4) { // If answer1 and answer4 have the same answers, then this can have same answer
-      //   alert(answer4);
-      // }
-      // if (answer1 != answer3 && answer2 != answer3) {
-      //   alert(answer3);
-      // }
-      //
