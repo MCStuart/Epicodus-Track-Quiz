8026797660c38e9f2f5c91542e750c38376e4a8c
Stuart McKay <stuart51994gmail.comgit config --global user.email stuart51994@gmail.com>
2019-03-29 17:24:39 -0700

    be a placeholder for submission, not working

--- js/scripts.js
+++ js/scripts.js
@@ -1,110 +1,106 @@
 $(document).ready(function() {
-  var cSharpCnt = 0;
   var javaCnt = 0;
   var rubyCnt = 0;
   var phpCnt = 0;
-  $("form#q1Input").change(function(){
+  $("form#q1Input").one("change", (function(){
     var answer1 = $("input[name='q1Answer']:checked").val();
     console.log(answer1);
     if (answer1 === "ruby") {
       rubyCnt++;
-    } else if (answer1 === "cSharp") {
-      cSharpCnt++;
     } else if (answer1 === "java") {
       javaCnt++;
     } else if (answer1 === "php") {
       phpCnt++;
     }
-  });
-  $("form#q2Input").change(function(){
+  }));
+  $("form#q2Input").one("change", (function(){
     var answer2 = $("input[name='q2Answer']:checked").val();
     console.log(answer2);
     if (answer2 === "ruby") {
       rubyCnt++;
-    } else if (answer2 === "cSharp") {
-      cSharpCnt++;
     } else if (answer2 === "java") {
       javaCnt++;
     } else if (answer2 === "php") {
       phpCnt++;
     }
-  });
-  $("form#q3Input").change(function(){
+  }));
+  $("form#q3Input").one("change", (function(){
     var answer3 = $("input[name='q3Answer']:checked").val();
     console.log(answer3);
     if (answer3 === "ruby") {
       rubyCnt++;
-    } else if (answer3 === "cSharp") {
-      cSharpCnt++;
     } else if (answer3 === "java") {
       javaCnt++;
     } else if (answer3 === "php") {
       phpCnt++;
     }
-  });
-  $("form#q4Input").change(function(){
+  }));
+  $("form#q4Input").one("change", (function(){
     var answer4 = $("input[name='q4Answer']:checked").val();
     console.log(answer4);
     if (answer4 === "ruby") {
       rubyCnt++;
-    } else if (answer4 === "cSharp") {
-      cSharpCnt++;
     } else if (answer4 === "java") {
       javaCnt++;
     } else if (answer4 === "php") {
       phpCnt++;
     }
-  });
-  $("form#q5Input").change(function() {
+  }));
+  $("form#q5Input").one("change", (function(){
     var answer5 = $("input[name='q5Answer']:checked").val();
     console.log(answer5);
     if (answer5 === "ruby") {
       rubyCnt++;
-    } else if (answer5 === "cSharp") {
-      cSharpCnt++;
     } else if (answer5 === "java") {
       javaCnt++;
     } else if (answer5 === "php") {
       phpCnt++;
     }
-  });
+  }));
   $("button.btn").click(function(){
-  console.log(javaCnt, rubyCnt, cSharpCnt, phpCnt);
+    console.log(javaCnt, rubyCnt, phpCnt);
+    function decideLanguage(javaCnt, rubyCnt, phpCnt) {
+      if (javaCnt >= 3) {
+        $("#javaCard").show();
+        $("#phpCard").hide();
+        $("#rubyCard").hide();
+        $(".questionField").hide();
+      }
+      if (phpCnt >= 3) {
+        $("#javaCard").hide();
+        $("#phpCard").show();
+        $("#rubyCard").hide();
+        $(".questionField").hide();
+      }
+      if (rubyCnt >= 3) {
+        $("#javaCard").hide();
+        $("#phpCard").hide();
+        $("#rubyCard").show();
+        $(".questionField").hide();
+      }
+      else if (javaCnt === 2 && phpCnt == 2) {
+        $("#javaCard").show();
+        $("#phpCard").show();
+        $("#rubyCard").hide();
+        $(".questionField").hide();
+      }
+      else if (javaCnt === 2 && ruby == 2) {
+        $("#javaCard").show();
+        $("#phpCard").hide();
+        $("#rubyCard").show();
+        $(".questionField").hide();
+      }
+      else if (rubyCnt === 2 && javaCnt == 2) {
+        $("#javaCard").show();
+        $("#phpCard").hide();
+        $("#rubyCard").show();
+        $(".questionField").hide();
+      }
+      else {
+        // $("#javaCard").show();
+        $(".questionField").hide();
+      }
+    };
   });
+  decideLanguage(javaCnt, phpCnt, rubyCnt);
 });
-
-function decideLanguage(cSharpCnt, javaCnt, rubyCnt, phpCnt) {
-  if (cSharpCnt >= 3) {
-    $("#cSharpCard").show();
-    $("#javaCard").hide();
-    $("#phpCard").hide();
-    $("#rubyCard").hide();
-    $(".questionField").hide();
-  }
-  if (javaCnt >= 3) {
-    $("#cSharpCard").hide();
-    $("#javaCard").show();
-    $("#phpCard").hide();
-    $("#rubyCard").hide();
-    $(".questionField").hide();
-  }
-  if (phpCnt >= 3) {
-    $("#cSharpCard").hide();
-    $("#javaCard").hide();
-    $("#phpCard").show();
-    $("#rubyCard").hide();
-    $(".questionField").hide();
-  }
-  if (rubyCnt >= 3) {
-    $("#cSharpCard").hide();
-    $("#javaCard").hide();
-    $("#phpCard").hide();
-    $("#rubyCard").show();
-    $(".questionField").hide();
-  }
-};
-  // else ;
-  //   $("#javaCard").show();
-  //   $(".questionField").hide();
-// };
-// decideLanguage(cSharpCnt, javaCnt, rubyCnt, phpCnt);
